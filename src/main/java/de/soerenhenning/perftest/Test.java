b27769fac5ee929b78256669ca2818770426b164
Sören Henning <post@soeren-henning.de>
2017-05-24 12:14:21 +0200

    added test object

--- src/main/java/de/soerenhenning/perftest/Test.java
+++ src/main/java/de/soerenhenning/perftest/Test.java
@@ -11,4 +11,8 @@ public interface Test {
 	public MachineIdentifier getMachineIdentifier();
 
 	public Map<String, TestAssertion> getTests();
+
+	public static Test of(final MachineIdentifier identifier, final Map<String, TestAssertion> tests) {
+		return new TestImpl(identifier, tests);
+	}
 }
