af9f11f1d9900ee19759acbad8b669f44693ae39
Sören Henning <post@soeren-henning.de>
2017-05-24 13:56:17 +0200

    added constructor for DismissIdentifier

--- src/main/java/de/soerenhenning/perftest/machine/identification/DismissIdentifier.java
+++ src/main/java/de/soerenhenning/perftest/machine/identification/DismissIdentifier.java
@@ -2,6 +2,9 @@ package de.soerenhenning.perftest.machine.identification;
 
 public class DismissIdentifier implements MachineIdentifier {
 
+	public DismissIdentifier(final String... paramaters) {
+	}
+
 	@Override
 	public boolean testMachine() {
 		return false;
