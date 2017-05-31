a666c3681c307eff5e23d04a4a070ec469cf826d
Sören Henning <post@soeren-henning.de>
2017-05-31 13:59:04 +0200

    added dummy stages for printing and csv export

---
+++ src/main/java/radargun/CSVExportStage.java
@@ -0,0 +1,13 @@
+package radargun;
+
+import radargun.test.result.TestResult;
+import teetime.framework.AbstractConsumerStage;
+
+public class CSVExportStage extends AbstractConsumerStage<TestResult> {
+
+	@Override
+	protected void execute(final TestResult result) throws Exception {
+		// TODO
+	}
+
+}
