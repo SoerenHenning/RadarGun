4263d28ca2143dfa0ab1fd25da3e411c029daab6
Sören Henning <SoerenHenning@users.noreply.github.com>
2017-06-14 13:28:54 +0200

    Set deployment branch to master

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'auto-deployment-final' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_67d30b645c9a_key -iv $encrypted_67d30b645c9a_iv -in deploy/codesigning.asc.enc -out deploy/codesigning.asc -d
     gpg --batch --fast-import deploy/codesigning.asc
 	mvn deploy --settings deploy/settings.xml -DperformRelease=true -DskipTests=true;
